28e1695604a120187f3b135f6748cc08e898635c
Žiga Stony <stonyziga@gmail.com>
2026-03-16 20:19:40 +0100

    Dodana funkcija množenja in meni možnost 3

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ def sestej(a, b):
 def odstej(a, b):
     return a - b
 
+def pomnozi(a, b):
+    return a * b
+
 print("Pozdrav! Mini kalkulator")
 print("1 = seštevanje")
 print("2 = odštevanje")
+print("3 - množenje")
 
-izbira = input("Kaj želiš narediti? (1 ali 2): ")
+izbira = input("Kaj želiš narediti? (1, 2 ali 3): ")
 
 if izbira == "1":
     x = float(input("Prvo število: "))
@@ -18,5 +22,9 @@ elif izbira == "2":
     x = float(input("Prvo število: "))
     y = float(input("Drugo število: "))
     print(f"Rezultat: {odstej(x, y)}")
+elif izbira == "3":
+    x = float(input("Prvo število: "))
+    y = float(input("Drugo število: "))
+    print(f"Rezultat: {pomnozi(x, y)}")
 else:
     print("Neveljavna izbira!")
